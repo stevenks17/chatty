94863d93219289690227e41a1cd71e5726c2fa89
stevenks17 <stevenks17@gmail.com>
2020-10-09 16:09:57 -0400

    Chat header created and styled

---
+++ src/ChatHeader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './ChatHeader.css'
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import EditLocationIcon from "@material-ui/icons/EditLocationRounded"
+import PeoplealtRoundedIcon from "@material-ui/icons/PeopleAltRounded"
+import SearchRoundedIcon from "@material-ui/icons/SearchRounded"
+import SendRoundedIcon from "@material-ui/icons/SendRounded"
+import HelpRoundedIcon from "@material-ui/icons/HelpRounded"
+
+
+function ChatHeader() {
+    return (
+        <div className = "chatHeader">
+            <div className="chatHeader__left">
+                <h3>
+                    <span className= "chatHeader__hash">#</span>
+                    Test Channel Name
+                </h3>
+            </div>
+
+            <div className="chatHeader__right">
+                <NotificationsIcon />
+                <EditLocationIcon />
+                <PeoplealtRoundedIcon />
+
+                <div className="chatHeader__search">
+                    <input placeholder='Search' />
+                    <SearchRoundedIcon />
+                </div>
+
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
+            </div>
+        </div>
+    )
+}
+
+export default ChatHeader
